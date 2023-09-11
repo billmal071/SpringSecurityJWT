ccfbf5d437ada2f2af60aae8539fcf32c3b9a8dc
WMalachy <theshinobi@pop-os.localdomain>
2023-09-11 22:05:41 +0100

    feat: code from Ali Bouali

---
+++ src/main/java/com/alibou/AlibouApplication.java
@@ -0,0 +1,13 @@
+package com.alibou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibouApplication.class, args);
+    }
+
+}
